f2a8a8b15876cb701a544e529ebecafeb8bc9b96
Akeyla <Akeyla@example.com>
2025-11-29 17:14:32 +0700

    perbaikan halaman

---
+++ uaswf/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '80px' }}>
+      <h1>Project Sistem Seminar</h1>
+      <p>React & Vite sudah berjalan!</p>
+    </div>
+  );
+}
+
+export default App;
